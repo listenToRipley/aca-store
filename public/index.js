a58b0e4a64c05539a29b60cefc543df15e109619
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-26 20:17:55 -0500

    first push

--- public/index.js
+++ public/index.js
@@ -1,2 +1,29 @@
 //stuff
 //more stuff
+'use strict'
+
+window.onload = () => {
+  getStore()
+  purchase()
+}
+
+let getStore = () => {
+  fetch('../db.json')
+  .then(function(response) {
+    // console.log('this is the response', response)
+    return response.json()
+  })
+  .then(function(json) {
+    // console.log('json', json)
+    let result = json.products
+    // console.log('json result :', result)
+    return result
+  })
+  .then(function(result) {
+    result.forEach(purchase)
+  })
+}
+
+let purchase = (item) => {
+  console.log('and each item in the array',item)
+}
